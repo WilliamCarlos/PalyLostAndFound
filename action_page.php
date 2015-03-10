202a014bab3bf02adda88e1e51a1c56a0ef3fd85
William Lee <masteratsoccer@gmail.com>
2015-03-10 13:12:58 -0700

    cleaned directory and header redirect

--- action_page.php
+++ action_page.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-
 <HTML>
 <title> Output page </title>
-
-
+</HTML>
 <?php
+
 $servername = "localhost";
 $username = "William";
 $password = "password";
@@ -19,38 +18,30 @@ $ownerLastName = $_GET['ownerLastName'];
 $additionalDetails = $_GET['additionalDetails'];
 $pwd = $_GET['pwd'];
 //uploaded file variable
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-//TRUNCATE TABLE names; will reset the names. 
-$sql = "INSERT INTO items (firstname)
-VALUES ('$firstname_input');";
-
 //IF PASSWORD EQUALS _____
 //MySQL databases columns -> var name
-//ADD TIMESTAMP IN ORDER TO IMPLEMENT SORT FUNCTIONALITY
+//ADD IMAGE URL LINK.. hm.. 
 $sql = "INSERT INTO items (firstname, lastname, articleType, articleColor, ownerFirstName, ownerLastName, additionalDetails)   
         VALUES ('$firstname_input', '$lastname_input', '$articleType', '$articleColor', '$ownerFirstName', '$ownerLastName', '$additionalDetails');";
     	//include all information in additionalDetails col of sql databsase to make a regex search easier. 
         //VALUES ('$firstname_input');";
         //VALUES('".$firstname."','".$lastname."','".$email."')";
-
-
 if ($conn->multi_query($sql) === TRUE) {
-    echo "New record created successfully";
+	//ADD HEADER REDIRECT?
+	//header("Location: http://mydomain.com/myOtherPage.php");
+	 /* Redirect browser */
+	//http://localhost:8888/lost/itemAdded.html
+    //echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    //echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
 //$conn->close();
+header("Location: http://localhost:8888/lost/itemAdded.html");
+exit();
 ?>
-
-
-</HTML>
-
-
